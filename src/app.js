56bdbc641a414727f3625d5d9a2645139f4f518c
Thom David <thomdavid74@gmail.com>
2019-02-15 10:38:11 -0800

    First commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+
+
+// Below is app functionality
+// const getData = async (url) => {
+//   const response = await fetch(url);
+//   const result = await response.json();
+//   console.log(result);
+// };
+
+// getData('https://jsonplaceholder.typicode.com/posts');
